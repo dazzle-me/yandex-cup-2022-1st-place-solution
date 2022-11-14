4644f1d9a5850de463b99cfef182a55217830083
Eduard Martynov <57013219+dazzle-me@users.noreply.github.com>
2022-11-15 01:33:06 +0300

    fix reqs

--- prepare_env.sh
+++ prepare_env.sh
@@ -1,3 +1,3 @@
-# pip install --pre torch --extra-index-url https://download.pytorch.org/whl/nightly/cu117
-# pip install -r requirements.txt
+pip install --pre torch --extra-index-url https://download.pytorch.org/whl/nightly/cu117
+pip install -r requirements.txt
 pip install tensorboard
